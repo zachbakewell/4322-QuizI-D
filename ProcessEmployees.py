ea6d81dcb966d7f44820fd0258bb8fffcfe98a5d
Zach Bakewell <you@example.com>
2023-02-13 15:08:07 -0600

    Increased Salaries for Marketing CSR's

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -11,20 +11,33 @@ Iternate through the dictionary to print out their name and thier new salary (as
 import csv
 
 #open the file
+infile = open('employee_data.csv', 'r', newline='')
 
+reader = csv.reader(infile, delimiter=',')
 
 
 
 #create an empty dictionary
-
+emp_sal = {}
 
 #use a loop to iterate through the csv file
-
-
+for row in reader:
     #check if the employee fits the search criteria
+    if row[3] == 'Marketing':
+        if row[4] == 'CSR':
+            first = row[1]
+            last = row[2]
+            name = first + ' ' + last
+            #print(name)
+            salary = row[5]
+            #print(salary)
+            emp_sal[name]= float(salary)
+        
+print()
 
-
-    
+#print the dictionary with original salaries before the salary increase
+for key,value in emp_sal.items():
+    print(f'Manager Name: {key} Current Salary: ${value:,.2f}')
 
 print()
 print('=========================================')
@@ -32,6 +45,10 @@ print()
 
 #iternate through the dictionary and print out the key and value as per printout
 
+#iterate through and multiply by 1.1 to get a 10 percent increase to each salary
+for key,value in emp_sal.items():
+    value *= 1.1
+    print(f'Manager Name: {key} Current Salary: ${value:,.2f}')
 
 
           
